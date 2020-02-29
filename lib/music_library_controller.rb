412d0fb0e7901b2424a880deaef02ec709164ac2
Biagio   Mendolia <mendolia2328@gmail.com>
2020-02-29 17:50:53 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -67,7 +67,7 @@ class MusicLibraryController
   end
   
   def play_song
-    puts ("Which song number would you like to play?")
+    puts "Which song number would you like to play?"
   end 
   
   
