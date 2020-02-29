651944197df4a1203ec740b4b09f1f2c088e028d
Biagio   Mendolia <mendolia2328@gmail.com>
2020-02-29 18:52:10 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -22,7 +22,19 @@ class MusicLibraryController
       puts "What would you like to do?"
       input = gets.chomp
       break if input == "exit" 
-    end 
+    end
+    
+    if input == "list songs"
+      list_songs  
+    elsif input == "list artists"
+      list_artists
+    elsif input == "list genres"
+      list_genres
+    elsif input == "list artist"
+      list_songs_by_artist
+    elsif input == "list genre"
+      list_songs_by_genre
+    end
   end
   
   def list_songs
