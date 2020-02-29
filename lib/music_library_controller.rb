5ff4f441dd74c81693a5a703cf4481715a0d489d
Biagio   Mendolia <mendolia2328@gmail.com>
2020-02-29 18:58:32 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -24,9 +24,20 @@ class MusicLibraryController
       break if input == "exit" 
     end
     
-    case input
-      when "list songs"
-        list_songs
+   case input
+         when "list songs"
+           list_songs
+         when "list artists"
+           list_artists
+         when "list genres"
+           list_genres
+         when "list artist"
+           list_songs_by_artist
+         when "list genre"
+           list_songs_by_genre
+         when "play song"
+           play_song
+         end
   end
   
   def list_songs
